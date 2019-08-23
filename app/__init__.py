49a5c8b3b65b764ee16a2f4a80a53cd1112e9dcb
Josh Isaacson <jsisaacs@iu.edu>
2019-08-23 16:26:40 -0400

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-test_data = pandas.read_csv('/Users/joshisaacson/Desktop/crypto-monitor-backend/test_data/crypto_data_dev.csv')
+test_data = pandas.read_csv('/Users/joshisaacson/Desktop/crypto_monitor_backend/test_data/crypto_data_dev.csv')
 test_data.to_sql(con=db.get_engine(), name="log", if_exists="replace")
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -9,14 +9,18 @@ import simplejson as json
 def health_check():
     try:
         query = Log.query.first()
-        response = {"status": "success", "code": 200}
-        return make_response(json.dumps(response), 200)
+        status = {"status": "success", "code": 200}
+        response = make_response(json.dumps(status), 200)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     except Exception as error:
         print(error)
-        response = {"status": "internal_server_error", "code": 500}
-        return make_response(json.dumps(response), 500)
+        status = {"status": "internal_server_error", "code": 500}
+        response = make_response(json.dumps(status), 500)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-@app.route('/crypto_data/<string:subreddit>')
+@app.route('/subreddit_data/<string:subreddit>')
 def crypto_data(subreddit):
     if subreddit in SUBREDDITS:
         all_logs = Log.query.filter(Log.subreddit==subreddit)
@@ -25,11 +29,15 @@ def crypto_data(subreddit):
         df.drop(['id', 'subreddit', 'currency_sentiment'], axis=1, inplace=True)
 
         df_dict = df.to_dict(orient="records")
-        status = {"status": "success", "code": 200}
 
-        response = {"response": status, "results": df_dict}
-        return make_response(json.dumps(response), 200)
+        status_and_results = {"subreddit": subreddit, "status": "success", "code": 200, "results": df_dict}
+        response = make_response(json.dumps(status_and_results), 200)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response      
     else:
-        response = {"status": "bad_request", "code": 400}
-        return make_response(json.dumps(response), 400)
+        status_and_results = {"subreddit": subreddit, "status": "bad_request", "code": 400}
+        response = make_response(json.dumps(status_and_results), 400)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
     
